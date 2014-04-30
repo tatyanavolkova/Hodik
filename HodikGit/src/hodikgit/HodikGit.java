06cae50066fad5d86a3d0bd2f68ba92d58c0c6b9
Tatyana Volkova <>
2014-04-30 13:48:29 +0400

    I added hello

--- HodikGit/src/hodikgit/HodikGit.java
+++ HodikGit/src/hodikgit/HodikGit.java
@@ -16,5 +16,6 @@ public class HodikGit {
     public static void main(String[] args) {
         // TODO code application logic here
         System.out.print("Hello from git!");
+        System.out.print("Hi!");
     }
 }
