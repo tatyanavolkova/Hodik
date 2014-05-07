db0818033f21c99ccb8ef25176f2e63a3dce524b
unl1m1t3d <>
2014-05-07 17:02:53 +0400

    Добавил игровое окно с параметрами и рисунками

--- HodikGit/src/hodikgit/Canvas.java
+++ HodikGit/src/hodikgit/Canvas.java
@@ -4,7 +4,12 @@
  */
 package hodikgit;
 import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import java.io.*;
+import javax.imageio.*;
 import javax.swing.*;
+
 /**
  *
  * @author user
@@ -16,16 +21,23 @@ public class Canvas extends JPanel {
     int fieldLeftTopX;
     int fieldLeftTopY;
     
+    BufferedImage imgObstacle;
+    
     Canvas(int fieldSizeX, int fieldSizeY, int fieldCellSize, int fieldLeftTopX, int fieldLeftTopY) {
         this.fieldSizeX = fieldSizeX;
         this.fieldSizeY = fieldSizeY;
         this.fieldCellSize = fieldCellSize;
         this.fieldLeftTopX = fieldLeftTopX;
         this.fieldLeftTopY = fieldLeftTopY;
+        try {
+            imgObstacle=ImageIO.read(new File("icons/hodik_leftdir.jpg"));
+        } catch (IOException e) {
+       }
     }
     public void paint(Graphics g) {
     super.paint(g);
     g.setColor(Color.black);
+    g.drawImage(imgObstacle,100,100,fieldCellSize,fieldCellSize,null);
     for(int i=0;i<=fieldSizeY;i++)
         g.drawLine(fieldLeftTopX, fieldLeftTopY+fieldCellSize*i, 
                 fieldLeftTopX+fieldCellSize*fieldSizeX, fieldLeftTopY + fieldCellSize*i);
@@ -33,4 +45,7 @@ public class Canvas extends JPanel {
         g.drawLine(fieldLeftTopX+fieldCellSize*i, fieldLeftTopY, 
                 fieldLeftTopX+fieldCellSize*i, fieldLeftTopY+fieldCellSize*fieldSizeY);
   }
+    void drawObstacle(Graphics g, int cellX, int cellY)
+    {
+    }
 }
