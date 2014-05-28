fe6927fe2a4c8d445e36bb146f3437fab0355bf6
unl1m1t3d <>
2014-05-28 16:43:17 +0400

    Добавил рисование робота и препятствий

--- HodikGit/src/hodikgit/Canvas.java
+++ HodikGit/src/hodikgit/Canvas.java
@@ -10,6 +10,7 @@ import java.io.*;
 import java.util.Map;
 import javax.imageio.*;
 import javax.swing.*;
+import java.util.ArrayList;
 
 /**
  *
@@ -23,16 +24,16 @@ public class Canvas extends JPanel {
     int fieldLeftTopY;
     Integrator integr;
     BufferedImage imgObstacle, imgRobot;
-    
+    ArrayList<Coordinate> obstacles;
     
     Canvas(int fieldSizeX, int fieldSizeY, int fieldCellSize, 
-            int fieldLeftTopX, int fieldLeftTopY, Integrator i) {
+            int fieldLeftTopX, int fieldLeftTopY, Integrator in) {
         this.fieldSizeX = fieldSizeX;
         this.fieldSizeY = fieldSizeY;
         this.fieldCellSize = fieldCellSize;
         this.fieldLeftTopX = fieldLeftTopX;
         this.fieldLeftTopY = fieldLeftTopY;
-        integr=i;
+        integr=in;
         try {
             imgObstacle=ImageIO.read(new File("icons/obstacle.jpg"));
         } catch (IOException e) {
@@ -41,23 +42,23 @@ public class Canvas extends JPanel {
             imgRobot=ImageIO.read(new File("icons/hodik.jpg"));
         } catch (IOException e) {
         }
-        for(Map.Entry<String, Robot> e: integr.rMap.entrySet()){  
-            //drawRobot(g,e.getValue().curr.coords.x,e.getValue().curr.coords.y);
-        }
-        for(Map.Entry<String, Robot> e: integr.rMap.entrySet()){  
-            //drawRobot(g,e.getValue().curr.coords.x,e.getValue().curr.coords.y);
-        }
     }
     public void paint(Graphics g) {
-    super.paint(g);
-    g.setColor(Color.black);
-    for(int i=0;i<=fieldSizeY;i++)
-        g.drawLine(fieldLeftTopX, fieldLeftTopY+fieldCellSize*i, 
-                fieldLeftTopX+fieldCellSize*fieldSizeX, fieldLeftTopY + fieldCellSize*i);
-    for(int i=0;i<=fieldSizeX;i++)
-        g.drawLine(fieldLeftTopX+fieldCellSize*i, fieldLeftTopY, 
-                fieldLeftTopX+fieldCellSize*i, fieldLeftTopY+fieldCellSize*fieldSizeY);
-    
+        super.paint(g);
+        g.setColor(Color.black);
+        for(int i=0;i<=fieldSizeY;i++)
+            g.drawLine(fieldLeftTopX, fieldLeftTopY+fieldCellSize*i, 
+                    fieldLeftTopX+fieldCellSize*fieldSizeX, fieldLeftTopY + fieldCellSize*i);
+        for(int i=0;i<=fieldSizeX;i++)
+            g.drawLine(fieldLeftTopX+fieldCellSize*i, fieldLeftTopY, 
+                    fieldLeftTopX+fieldCellSize*i, fieldLeftTopY+fieldCellSize*fieldSizeY);
+
+        for(Map.Entry<String, Robot> e: integr.rMap.entrySet()){  
+            //drawRobot(g,e.getValue().curr.coords.x,e.getValue().curr.coords.y);
+        }
+        for(int i=0;i<obstacles.size();i++){  
+            drawObstacle(g, obstacles.get(i).x, obstacles.get(i).x);
+        }
     }
     void drawObstacle(Graphics g, int cellX, int cellY)
     {
