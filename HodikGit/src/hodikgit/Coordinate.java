34a15ff9724d46c04b7d085502f2c8c745cf9791
Арсен <@192.168.249.5>
2014-05-28 13:42:18 +0400

    Добавлен класс Coordinate

---
+++ HodikGit/src/hodikgit/Coordinate.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hodikgit;
+
+/**
+ *
+ * @author Арсен
+ */
+public class Coordinate {
+    int x, y, p;
+    public Coordinate (int a, int b, int c){
+        this.x = a;
+        this.y = b;
+        this.p = c;
+    }
+}
