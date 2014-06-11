35fb853fe488c83c63494d069cfc263166a03d5f
Белое Безмозглое < @192.168.249.56>
2014-06-11 14:50:45 +0400

    Интерпретатор :)

--- HodikGit/src/hodikgit/Interpretator.java
+++ HodikGit/src/hodikgit/Interpretator.java
@@ -9,5 +9,38 @@ package hodikgit;
  * @author user
  */
 public class Interpretator {
+    Coordinate c;
+
+    public Interpretator(){
+        this.c.x=0;
+        this.c.y=0;
+        this.c.p=0;
+    }
+    public Interpretator (Coordinate C){
+        this.c=C;
+    }
     
+    public void Run (String S){
+        String Command[]=new String[50];
+        int i=0;
+        while (Command[i]!=null){
+            if (Command[i].equals("Step")) this.Step();
+            if (Command[i].equals("Rotate")) this.Rotate();
+            i++;
+        }
+
+    }
+    
+    public void Step(){
+       if(this.c.p==0) this.c.y=this.c.y+1;
+       if(this.c.p==1) this.c.x=this.c.x+1;
+       if(this.c.p==2) this.c.y=this.c.y-1;
+       if(this.c.p==3) this.c.x=this.c.x-1;
+    }
+    
+    public void Rotate(){
+        this.c.p=(this.c.p+1)%4;        
+    }
+   
+   
 }
